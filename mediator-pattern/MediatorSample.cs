f5976412d1e65c40b5100c3059a803477f2d3559
hakanmarthidir <hakan_hidir@hotmail.com>
2021-09-19 19:26:40 +0200

    mediator pattern sample

---
+++ mediator-pattern/MediatorSample.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mediator_pattern
+{
+    //1
+
+    public interface IShare
+    {
+        void Register(IDevice device);
+        void SendFile(string from, string to, string filePath);
+    }
+
+
+    public interface IDevice
+    {
+        string Name { get; set; }
+        DeviceType Type { get; set; }
+        IShare ShareSystem { get; set; }
+
+        void Send(string to, string filePath);
+        void Receive(string from, string filePath);
+    }
+
+    public enum DeviceType
+    {
+        Android,
+        IOS,
+        Windows,
+        Ubuntu
+    }
+
+    //2
+
+    public class FileShareSystem : IShare
+    {
+        private Dictionary<string, IDevice> _deviceList = new Dictionary<string, IDevice>();
+        public void Register(IDevice device)
+        {
+            if (!this._deviceList.ContainsKey(device.Name))
+                this._deviceList.Add(device.Name, device);
+        }
+
+        public void SendFile(string from, string to, string filePath)
+        {
+            IDevice device = this._deviceList[to];
+            if (device != null)
+            {
+                device.Receive(from, filePath);
+            }
+
+        }
+    }
+
+    // 3
+
+    public class WindowsDevice : IDevice
+    {
+        public string Name { get ; set ; }
+        public DeviceType Type { get; set; }
+        public IShare ShareSystem { get; set; }
+
+        public void Receive(string from, string filePath)
+        {
+            Console.WriteLine($"This is {Name} I got a file From : {from}, File : {filePath}");
+        }
+
+        public void Send(string to, string filePath)
+        {
+            this.ShareSystem.SendFile(Name, to, filePath);
+        }
+    }
+
+
+
+}
--- mediator-pattern/Program.cs
+++ mediator-pattern/Program.cs
@@ -6,7 +6,22 @@ namespace mediator_pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileShareSystem fileShareSystem = new FileShareSystem();
+
+            IDevice device1 = new WindowsDevice() { Name="Win10", Type= DeviceType.Windows, ShareSystem = fileShareSystem };
+            IDevice device2 = new WindowsDevice() { Name = "Win11", Type = DeviceType.Windows, ShareSystem = fileShareSystem };
+            IDevice device3 = new WindowsDevice() { Name = "MyLaptop", Type = DeviceType.Windows, ShareSystem = fileShareSystem };
+
+            fileShareSystem.Register(device1);
+            fileShareSystem.Register(device2);
+            fileShareSystem.Register(device3);
+
+            device1.Send(device2.Name, "C:\\abcd.txt");
+            device3.Send(device1.Name, "C:\\eeee.txt");
+            device2.Send(device3.Name, "C:\\dddd.txt");
+
+
         }
     }
+
 }

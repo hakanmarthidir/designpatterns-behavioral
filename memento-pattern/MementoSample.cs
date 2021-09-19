4f3449d1bb86becf4a22bf8f2cf17e280dbb6b6d
hakanmarthidir <hakan_hidir@hotmail.com>
2021-09-19 20:01:09 +0200

    memento pattern samples

---
+++ memento-pattern/MementoSample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memento_pattern
+{
+
+    public class GodOfWar
+    {
+        public int EarnPoint { get; set; }
+        public byte Level { get; set; }
+        public string MapPoint { get; set; }
+
+        public GodOfWarMemento Save()
+        {
+            GodOfWarMemento memento = new() { EarnPoint = this.EarnPoint, Level = this.Level, MapPoint = this.MapPoint };
+            return memento;
+        }
+
+        public void LoadSavedPoint(GodOfWarMemento memento)
+        {
+            this.EarnPoint = memento.EarnPoint;
+            this.Level = memento.Level;
+            this.MapPoint = memento.MapPoint;
+        }
+
+    }
+
+    public class GodOfWarMemento
+    {
+        public int EarnPoint { get; set; }
+        public byte Level { get; set; }
+        public string MapPoint { get; set; }
+    }
+
+    public class MementoCareTaker
+    {
+        public GodOfWarMemento GodOfWarMemento { get; set; }
+        //you can use the Stack or other Lists..
+    }
+
+
+
+}
--- memento-pattern/Program.cs
+++ memento-pattern/Program.cs
@@ -6,7 +6,31 @@ namespace memento_pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            GodOfWar godOfWar = new GodOfWar() { Level=1, EarnPoint= 0, MapPoint="Home" };
+            Console.WriteLine($"Initial Values {godOfWar.Level} { godOfWar.EarnPoint} {godOfWar.MapPoint}");
+
+            //After Played
+            godOfWar.Level = 3;
+            godOfWar.EarnPoint = 15000;
+            godOfWar.MapPoint = "Mountain";
+            Console.WriteLine($"Current Values {godOfWar.Level} { godOfWar.EarnPoint} {godOfWar.MapPoint}");
+
+            //Save the Game
+            MementoCareTaker mementoCareTaker = new MementoCareTaker();
+            mementoCareTaker.GodOfWarMemento = godOfWar.Save();
+            Console.WriteLine("Game Saved...");
+
+            //After Played
+            godOfWar.Level = 5;
+            godOfWar.EarnPoint = 23000;
+            godOfWar.MapPoint = "Forest";
+            Console.WriteLine($"Current Values {godOfWar.Level} { godOfWar.EarnPoint} {godOfWar.MapPoint}");
+
+            //Load Check Point
+            godOfWar.LoadSavedPoint(mementoCareTaker.GodOfWarMemento);
+            Console.WriteLine($"Reload Values {godOfWar.Level} { godOfWar.EarnPoint} {godOfWar.MapPoint}");
+
         }
     }
 }

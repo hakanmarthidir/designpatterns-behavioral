add1b4e665fecb1909e25a9a5cec92462197310d
hakan hidir <hakan_hidir@hotmail.com>
2021-09-21 01:16:50 +0200

    observer pattern sample

---
+++ observer-pattern/ObserverSample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace observer_pattern
+{
+
+    //1
+    public interface ISubscriber
+    {
+        string Name { get; set; }
+        void UpdateData(string message);
+    }
+
+    public interface IPublisher
+    {
+        string Name { get; set; }
+        void Subscribe(ISubscriber member);
+        void UnSubscribe(ISubscriber member);
+        void Notify(string message);
+    }
+
+    //2
+
+    public class YoutubeChannel : IPublisher
+    {
+        public string Name { get; set; }
+        Dictionary<string, ISubscriber> _subscribers;
+
+        public YoutubeChannel()
+        {
+            this._subscribers = new Dictionary<string, ISubscriber>();
+        }
+
+
+        public void PublishVideo()
+        {
+            //publishing operations and after that send message to followers
+
+            this.Notify($" {this.Name} channel was published a new video.");
+        }
+
+        public void Notify(string message)
+        {
+            foreach (var subscriber in this._subscribers.Values)
+            {
+                subscriber.UpdateData(message);
+            }
+        }
+
+        public void Subscribe(ISubscriber member)
+        {
+            if (!this._subscribers.ContainsKey(member.Name))
+            {
+                this._subscribers.Add(member.Name, member);
+            }
+        }
+
+        public void UnSubscribe(ISubscriber member)
+        {
+            if (this._subscribers.ContainsKey(member.Name))
+            {
+                this._subscribers.Remove(member.Name);
+            }
+        }
+    }
+
+    //3
+    public class YoutubeMember : ISubscriber
+    {
+        public string Name { get; set; }
+
+        public void UpdateData(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+}
--- observer-pattern/Program.cs
+++ observer-pattern/Program.cs
@@ -6,7 +6,21 @@ namespace observer_pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            YoutubeChannel youtubeChannel = new YoutubeChannel() { Name = "Science 101" };
+
+
+            YoutubeMember member1 = new YoutubeMember() { Name = "User A" };
+            YoutubeMember member2 = new YoutubeMember() { Name = "User B" };
+            YoutubeMember member3 = new YoutubeMember() { Name = "User C" };
+            YoutubeMember member4 = new YoutubeMember() { Name = "User D" };
+
+            youtubeChannel.Subscribe(member1);
+            youtubeChannel.Subscribe(member2);
+            youtubeChannel.Subscribe(member3);
+            youtubeChannel.Subscribe(member4);
+
+            youtubeChannel.PublishVideo();
+
         }
     }
 }
